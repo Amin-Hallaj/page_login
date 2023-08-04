25bb0796296ef6336765b2abb53e51f351684861
Amin-Hallaj <aminhallajm@gmail.com>
2023-08-04 16:52:57 +0330

    ویرایش مدل

--- login_page/models.py
+++ login_page/models.py
@@ -9,8 +9,6 @@ class MemberRegistration(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE, related_name="member_registration")
     national_code=models.CharField(max_length=10)
     first_name_last_name=models.CharField(max_length=200)
-    gmail=models.CharField(max_length=100)
-    phone_number=models.CharField(max_length=13)
 
     def __str__(self):
         return self.fullname
