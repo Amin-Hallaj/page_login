07fbf88a4c738f7f7b295f8afb4f45f274c47afa
Amin-Hallaj <aminhallajm@gmail.com>
2023-08-04 16:40:46 +0330

    ساخت تابع ثبت نام کاربر

--- login_page/views.py
+++ login_page/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , get_object_or_404 , redirect
 from django.contrib.auth import login , authenticate , logout 
 from django.contrib import messages
+from django.contrib.auth.models import  User , Permission
+from .models import MemberRegistration
 
 
 def master_index(request):
@@ -16,6 +18,7 @@ def master_login(request):
 def master_login_submit(request):
 
     if request.method=="POST":
+
         username=request.POST.get('username')
         password=request.POST.get('password')
         user=authenticate(username=username , password=password)
@@ -45,6 +48,7 @@ def operator_login(request ):
 def operator_login_submit(request):
 
     if request.method == "POST":
+
         username=request.POST.get('national_id')
         member_pass=request.POST.get('member_pass')
         user=authenticate(username=username, password=member_pass)
@@ -52,7 +56,7 @@ def operator_login_submit(request):
         if user is not None:
             login(request , user)
             messages.success(request, f"{request.user.user_members} خوش اومدین.")
-            return redirect("operator_panel")
+            return redirect("master_index")
 
         else:
             messages.error(request,"نام کاربری یا رمز عبور نامعتبر است.")
@@ -61,7 +65,37 @@ def operator_login_submit(request):
     return redirect("operator_login" )
 
 
+def member_registration(request):
+
+    if not request.user.is_authenticated:
+        return redirect('master_login')
+    
+    return render(request , 'master/login.html', {})
+
+
+def member_registration_submit(request):
+
+    if not request.user.is_authenticated:
+        return redirect('master_login')
+    
+    if request.method=='POST':
+
+        national_code=request.POST.get('national_code')
+        first_name_last_name=request.POST.get('first_name_last_name')
+        password_new=request.POST.get('password_new')
+
+        user=User.objects.create_user(username=national_code , password=password_new)
+
+    MemberRegistration.objects.create(
+            user=user,
+            national_code=national_code ,
+            first_name_last_name=first_name_last_name ,
+            )
+    
+    return redirect("member_registration")
+
+
 def operator_logout(request):
 
     logout(request)
-    return redirect("operator_login")
+    return redirect("master_login")
